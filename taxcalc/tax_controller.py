7e4a34dd94d72397f345f7fd6aaef2212b4b6297
rik-wachner <37379310+rik-wachner@users.noreply.github.com>
2022-12-29 16:37:31 +0100

    Create 'Add Product' functionality

--- taxcalc/tax_controller.py
+++ taxcalc/tax_controller.py
@@ -7,6 +7,9 @@ class TaxController:
         self.view = view
         self.taxProducts: list[TaxProduct] = []
 
+        # Add reference to UI to get user events
+        self.view.add_controller_listener(self)
+
     @staticmethod
     def _check_product_information(product_information: dict):
         pass
@@ -16,9 +19,38 @@ class TaxController:
         pass
 
     def add_product(self, product_information: dict):
-        pass
+        """
+        Adds a new product to the receipt with the following structure:
 
-    def calculate_receipt(self):
+        product_information: dict = {
+            "product_name": <the product name>,
+            "product_quantity": <the product quantity>,
+            "product_price": <the product price>,
+            "product_basic_tax": <the product basic tax rate>,
+            "product_import_state": <the product import state>
+        }
+
+        :param product_information: Map with the required product information to create a new TaxProduct object
+        :type product_information: dict
+        :return: None
+        :rtype: None
+        """
+        try:
+            self._check_product_information(product_information)
+            new_product: TaxProduct = TaxProduct(
+                name=str(product_information["product_name"]),
+                quantity=int(product_information["product_quantity"]),
+                price=float(product_information["product_price"]),
+                basic_tax=int(product_information["product_basic_tax"]),
+                import_state=bool(product_information["product_import_state"])
+            )
+            self.taxProducts.append(new_product)
+            # new_product.getProductInformation() == product_information
+            # @ TODO Inform UI about change
+        except Exception as err:
+            pass
+
+    def calculate_receipt(self, a, b):
         pass
 
     def calculate_total(self) -> dict:
--- taxcalc/tax_ui_app.py
+++ taxcalc/tax_ui_app.py
@@ -139,8 +139,35 @@ class TaxUIApp(customtkinter.CTk):
         )
         self.show_receipt_button.grid(row=1, column=0, padx=(10, 10), pady=(10, 10), sticky="nsew")
 
+    def add_controller_listener(self, controller: TaxController):
+        """
+        Register a controller to be able to pass click events and information
+        :param controller: Controller for managing the data transfer between ui and data
+        :type controller: TaxController
+        :return: None
+        :rtype: None
+        """
+        self.connected_controller = controller
+
     def add_product_click(self):
-        pass
+        """
+        Function gets called after the 'Add Product' button were pressed
+        and adds a new product with the information in the input fields
+        :return: None
+        :rtype: None
+        """
+        chosen_basic_tax_option = self.basic_tax.get()
+        basic_tax = 0
+        #position_of_tax_translation = tax_translation.index(chosen_basic_tax_option)
+        if chosen_basic_tax_option == "10%":
+            basic_tax = 10
+        self.add_new_product(
+            product_name=self.product_name.get(),
+            product_quantity=self.product_quantity.get(),
+            product_price=self.product_price.get(),
+            product_basic_tax=basic_tax,
+            product_import_state=bool(self.import_checkbox.get())
+        )
 
     def reset_calculation_click(self):
         pass
@@ -148,8 +175,37 @@ class TaxUIApp(customtkinter.CTk):
     def show_receipt_click(self):
         pass
 
-    def add_new_product(self):
-        pass
+    def add_new_product(
+            self,
+            product_name: str,
+            product_quantity: int,
+            product_price: float,
+            product_basic_tax: int,
+            product_import_state: bool
+    ):
+        """
+        Calls the tax controller to add a product with the given information
+        :param product_name: Product name
+        :type product_name: str
+        :param product_quantity: Quantity of that product
+        :type product_quantity: int
+        :param product_price: Price of that product
+        :type product_price: float
+        :param product_basic_tax: The chosen basic tax rate (either 0 or 10)
+        :type product_basic_tax: int
+        :param product_import_state: Indicates if the product were imported and need the 5% import duty
+        :type product_import_state: bool
+        :return: None
+        :rtype: None
+        """
+        product_information: dict = {
+            "product_name": product_name,
+            "product_quantity": product_quantity,
+            "product_price": product_price,
+            "product_basic_tax": product_basic_tax,
+            "product_import_state": product_import_state
+        }
+        self.connected_controller.add_product(product_information)
 
     def show_receipt(self):
         pass
